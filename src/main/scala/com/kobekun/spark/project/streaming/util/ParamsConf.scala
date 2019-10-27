be122288443ce6171a62a438104a8fd9a15219c5
kobekun <kobekun881105@126.com>
2019-10-27 23:22:46 +0800

    项目参数读取

--- src/main/scala/com/kobekun/spark/project/streaming/util/ParamsConf.scala
+++ src/main/scala/com/kobekun/spark/project/streaming/util/ParamsConf.scala
@@ -1,6 +1,7 @@
 package com.kobekun.spark.project.streaming.util
 
 import com.typesafe.config.ConfigFactory
+import org.apache.kafka.common.serialization.StringDeserializer
 
 /**
   * 项目参数读取类
@@ -9,18 +10,27 @@ object ParamsConf {
 
   private lazy val config = ConfigFactory.load()
 
-  val topic = config.getString("kafka.topic")
+  val topic = config.getString("kafka.topic").split(",")
   val groupId = config.getString("kafka.group.id")
-  val brokerList = config.getString("kafka.broker.list")
+  val brokers = config.getString("kafka.broker.list")
 
   val redisHost = config.getString("redis.host")
-  val redisDb = config.getString("redis.db")
+  val redisDb = config.getInt("redis.db")
+
+  val kafkaParams = Map[String, Object](
+    "bootstrap.servers" -> brokers,
+    "key.deserializer" -> classOf[StringDeserializer],
+    "value.deserializer" -> classOf[StringDeserializer],
+    "group.id" -> groupId,
+    "auto.offset.reset" -> "latest",
+    "enable.auto.commit" -> (false: java.lang.Boolean)
+  )
 
   def main(args: Array[String]): Unit = {
 
     println(ParamsConf.topic)
     println(ParamsConf.groupId)
-    println(ParamsConf.brokerList)
+    println(ParamsConf.brokers)
 
     println(ParamsConf.redisHost)
     println(ParamsConf.redisDb)
